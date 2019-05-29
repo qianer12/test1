a66908bd30e91a86d0350c7f57dec057916fbaf0
qianer12 <qianer90@qq.com>
2019-05-29 15:23:55 +0800

    全局事务配置

--- src/main/java/com/test1/config/TransactionManagerConfig.java
+++ src/main/java/com/test1/config/TransactionManagerConfig.java
@@ -25,10 +25,10 @@ import java.util.Map;
 public class TransactionManagerConfig {
 
     private static final int TX_METHOD_TIMEOUT = 5;
-    /*
-    定义切点变量：拦截test.spring包下所有类的所有方法,返回值类型任意的方法
-     */
-    private static final String AOP_POINTCUT_EXPRESSION = "execution (* com.test1.service..*(..))";
+
+    //定义切点变量：拦截com.test1.service包下所有类的所有方法,返回值类型任意的方法
+    private static final String AOP_POINTCUT_EXPRESSION = "execution (* com.test1.service.*.*(..))";
+
     @Autowired
     private PlatformTransactionManager transactionManager;
 
@@ -38,46 +38,37 @@ public class TransactionManagerConfig {
      */
     @Bean
     public TransactionInterceptor TxAdvice() {
-        /*
-        事务管理规则，声明具备事务管理的方法名
-         */
+
+        //事务管理规则，声明具备事务管理的方法名
         NameMatchTransactionAttributeSource source = new NameMatchTransactionAttributeSource();
+
         /*
         只读事物、不做更新删除等;
         当前存在事务就用当前的事务，当前不存在事务就创建一个新的事务
          */
         RuleBasedTransactionAttribute readOnlyRule = new RuleBasedTransactionAttribute();
-        /*
-        设置当前事务是否为只读事务，true为只读
-         */
+        //设置当前事务是否为只读事务，true为只读
         readOnlyRule.setReadOnly(true);
-        /*
-        TransactionDefinition 定义事务的隔离级别；
-        PROPAGATION_NOT_SUPPORTED事务传播级别5，以非事务运行，如果当前存在事务，则把当前事务挂起
-         */
+        //TransactionDefinition 定义事务的隔离级别；
+        //PROPAGATION_NOT_SUPPORTED事务传播级别5，以非事务运行，如果当前存在事务，则把当前事务挂起
         readOnlyRule.setPropagationBehavior(TransactionDefinition.PROPAGATION_NOT_SUPPORTED);
 
         RuleBasedTransactionAttribute requireRule = new RuleBasedTransactionAttribute();
-        /*
-        抛出异常后执行切点回滚
-         */
+        //抛出异常后执行切点回滚
         requireRule.setRollbackRules(Collections.singletonList(new RollbackRuleAttribute(Exception.class)));
-        /*
-        PROPAGATION_REQUIRED:事务隔离性为1，若当前存在事务，则加入该事务；如果当前没有事务，则创建一个新的事务。这是默认值。
-         */
+        //PROPAGATION_REQUIRED:事务隔离性为1，若当前存在事务，则加入该事务；如果当前没有事务，则创建一个新的事务。
         requireRule.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
-        /*
-        设置事务失效时间，如果超过5秒，则回滚事务
-         */
+        //设置事务失效时间，如果超过5秒，则回滚事务
         requireRule.setTimeout(TX_METHOD_TIMEOUT);
-        Map<String, TransactionAttribute> txMap = new HashMap<>();
 
+        Map<String, TransactionAttribute> txMap = new HashMap<>();
         txMap.put("add*", requireRule);
         txMap.put("save*", requireRule);
         txMap.put("insert*", requireRule);
         txMap.put("update*", requireRule);
         txMap.put("delete*", requireRule);
         txMap.put("remove*", requireRule);
+        txMap.put("transfer*", requireRule);
         txMap.put("get*", readOnlyRule);
         txMap.put("query*", readOnlyRule);
         txMap.put("find*", readOnlyRule);
@@ -98,13 +89,10 @@ public class TransactionManagerConfig {
         切面（Aspect）：切面就是通知和切入点的结合。通知和切入点共同定义了关于切面的全部内容——它的功能、在何时和何地完成其功能。
          */
         AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
-        /*
-        声明和设置需要拦截的方法,用切点语言描写
-         */
+        //声明和设置需要拦截的方法,用切点语言描写
         pointcut.setExpression(AOP_POINTCUT_EXPRESSION);
-        /*
-        设置切面=切点pointcut+通知TxAdvice
-         */
+
+        //设置切面=切点pointcut+通知TxAdvice
         return new DefaultPointcutAdvisor(pointcut, TxAdvice());
     }
 }
